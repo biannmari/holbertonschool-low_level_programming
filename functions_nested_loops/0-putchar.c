ea351a0885097b09ba5feb9d5fca5406dac80c65
biannmari <marisolmcabrera@gmail.com>
2024-07-18 11:02:20 -0400

    Excersise 0 completed2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- *Print _putchar character by character
+ *main - Entry point
+ *Description: print _putchar followed by new line
+ *Return: 0 (success)
  */
 
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
 
 }
