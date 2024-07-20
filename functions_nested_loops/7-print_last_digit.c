9a5e82b51f24d65b89d8481b6a8110d71f568dc0
biannmari <marisolmcabrera@gmail.com>
2024-07-20 19:58:59 -0400

    Prints the last digit of a number-betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,15 @@
 *
 * Return: the value of the last digit
 */
-int print_last_digit(int n) {
-int r = n % 10;
+int print_last_digit(int n)
+
+{
+int (r = n % 10);
 
 if (r < 0)
 r = -r;
 
 _putchar(r + '0');
 
-return r;
+return (r);
 }
