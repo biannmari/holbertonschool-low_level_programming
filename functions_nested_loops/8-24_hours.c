383c40a7b3c9c9f4563f11261be48961475f15c0
biannmari <marisolmcabrera@gmail.com>
2024-07-22 10:13:58 -0400

    Everything regarding 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,12 +3,12 @@
 /**
 * jack_bauer - prints every minute of the day of Jack Bauer.
 *
-* For each minute,
-* it prints the time in the format HH:MM.
+* For each minute, it prints the time in the format HH:MM.
 */
 
 
 void jack_bauer(void)
+
 {
 int hours = 0;
 int minutes = 0;
@@ -20,9 +20,10 @@ while (minutes < 60)
 {
 printf("%02d:%02d\n", hours, minutes);
 minutes++;
-
 }
+
 minutes = 0;
 hours++;
+
 }
 }
