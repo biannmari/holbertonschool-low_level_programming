ece773a3e2d360d3e298543a402cab447c975372
biannmari <marisolmcabrera@gmail.com>
2024-07-21 21:40:20 -0400

    Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59, betty style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer.
+*
+* For each minute,
+* it prints the time in the format HH:MM.
+*/
+
+
 void jack_bauer(void)
 {
 int hours = 0;
 int minutes = 0;
-int timer = 0;
 
 while (hours < 24)
 {
@@ -13,15 +20,9 @@ while (minutes < 60)
 {
 printf("%02d:%02d\n", hours, minutes);
 minutes++;
-timer++;
-if (timer >= 60)
-
-{
-timer = 0;
-hours++;
-}
 
 }
 minutes = 0;
+hours++;
 }
 }
