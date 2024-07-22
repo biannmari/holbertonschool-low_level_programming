7bf60a36732eedac738956d36e329941c086f2a6
biannmari <marisolmcabrera@gmail.com>
2024-07-22 18:02:17 -0400

    Files for test removed

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* add - sums 2 integers.
+*
+* @a: first integer.
+* @b: second integer.
+*
+* return the sum of a and b.
+*/
+
+int add(int a, int b)
+{
+	int resutl;
+
+	result = a + b;
+	return (result);
+}
